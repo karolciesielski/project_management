fea20e855da0254e1bf43bc262ceb6bf54f8dfd7
Piotr Dobrodziej <pidobr@wp.pl>
2019-03-24 09:29:59 +0100

    updated homePage.html and create class task

--- src/main/java/pl/management/project/project_management/model/Task.java
+++ src/main/java/pl/management/project/project_management/model/Task.java
@@ -1,4 +1,30 @@
 package pl.management.project.project_management.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name="task")
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @NotBlank(message = "The name may not be blank")
+    protected String name;
+
+    private String description;
+    private int sprint;
+    private int weight;
+    private int storyPoints;
+    private int progress;
+    private long user;
+
+
 }
